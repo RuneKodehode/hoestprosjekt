d626c987e8fcaaf80170f77a734d1b0c5f6fbc0c
RuneKodehode <112623367+RuneKodehode@users.noreply.github.com>
2022-10-10 18:17:01 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-asdasd;
-asdasdas;
+let drop = 0;
+let navbarOne = document.getElementById("dropDownOne");
+let navbarTwo = document.getElementById("dropDownTwo");
+
+
+function nav() {
+  if (!drop) {
+    navbarOne.style.opacity = "1";
+    navbarTwo.style.opacity = "1";
+    navbarOne.style.padding = "20px 20px 20px 10px";
+    navbarTwo.style.padding = "20px 20px 20px 10px";
+    drop = 1;
+  } else {
+    navbarOne.style.padding = "0px";
+    navbarTwo.style.padding = "0px";
+    navbarOne.style.opacity = 0;
+    navbarTwo.style.opacity = 0;
+    drop = 0;
+  }
+}
+
+function about() {}
+function contact() {}
